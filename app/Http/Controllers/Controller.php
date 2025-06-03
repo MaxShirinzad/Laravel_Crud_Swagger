899196b2a5def684b27a7ff642068ba34929bd6d
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2025-06-04 01:48:12 +0330

    Swagger install and config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Laravel API",
+ *      description="Laravel API Documentation",
+ * )
+ *
+ * @OA\SecurityScheme(
+ *   securityScheme="bearerAuth",
+ *   type="http",
+ *   scheme="bearer"
+ * )
+ *
+ * @OA\Server(url=L5_SWAGGER_CONST_HOST)
+ **/
 abstract class Controller
 {
     //
